b005365b27bbf9337c5f84b35f88e6665f2447ba
Matthias Jung <m.jung@uni-wuerzburg.de>
2024-10-11 16:19:26 +0200

    first shader implementation

--- src/gui.cpp
+++ src/gui.cpp
@@ -3,9 +3,11 @@
 gui::gui(
     std::function<bool()> connectCallback,
     std::function<bool()> isConnectedCallback
-) {
+) : ringBuffer(256) {
     this->connectCallback = connectCallback;
     this->isConnectedCallback = isConnectedCallback;
+
+    initWaterfall();
 }
 
 void gui::render() {
@@ -44,34 +46,62 @@ void gui::renderWaterfall(float width, float height, float xoffset) {
     ImGui::Begin("Waterfall", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse);
 
         window = ImGui::GetCurrentWindow();
-
         auto widgetPos = ImGui::GetWindowContentRegionMin();
         auto widgetEndPos = ImGui::GetWindowContentRegionMax();
         widgetPos.x += window->Pos.x;
         widgetPos.y += window->Pos.y;
-        widgetEndPos.x += window->Pos.x - 4; // Padding
+        widgetEndPos.x += window->Pos.x; // Padding
         widgetEndPos.y += window->Pos.y;
         auto widgetSize = ImVec2(widgetEndPos.x - widgetPos.x, widgetEndPos.y - widgetPos.y);
 
-        //if (selectedVFO == "" && vfos.size() > 0) {
-        //    selectFirstVFO();
+        std::array<u_int8_t, 4096> data;
+        std::generate(data.begin(), data.end(), []() { return static_cast<u_int8_t>(rand() % 256); });
+        //for(int i = 0; i < 4096; i++) {
+        //    data[i] = static_cast<u_int8_t>(i % 256);
         //}
 
-        //if (widgetPos.x != lastWidgetPos.x || widgetPos.y != lastWidgetPos.y) {
-        //    lastWidgetPos = widgetPos;
-        //    onPositionChange();
-        //}
-        //if (widgetSize.x != lastWidgetSize.x || widgetSize.y != lastWidgetSize.y) {
-        //    lastWidgetSize = widgetSize;
-        //    onResize();
-        //}
+        // Define a gradient lookup table
+        std::array<ImVec4, 256> gradient;
+        for (int i = 0; i < 256; ++i) {
+            gradient[i] = ImVec4(
+                i / 255.0f, // Red
+                0.0f, // Green
+                (255 - i) / 255.0f, // Blue
+                1.0f
+            ); // Example gradient from red to blue
+        }
 
-        window->DrawList->AddRectFilled(widgetPos, widgetEndPos, IM_COL32( 0, 0, 0, 255 ));
+        // Add new data to the ring buffer
+        ringBuffer[waterfallIndex] = data;
+        waterfallIndex = (waterfallIndex + 1) % ringBuffer.size();
+
+        // Create a texture from the ring buffer
+        std::vector<u_int8_t> textureData(4096 * 256 * 3);
+        for (int i = 0; i < 256; ++i) {
+            for (int j = 0; j < 4096; ++j) {
+                int index = ((waterfallIndex + i) % 256) * 4096 + j;
+                u_int8_t value = ringBuffer[index / 4096][index % 4096];
+                ImVec4 color = gradient[value];
+                textureData[(i * 4096 + j) * 3 + 0] = static_cast<u_int8_t>(color.x * 255); // Red
+                textureData[(i * 4096 + j) * 3 + 1] = static_cast<u_int8_t>(color.y * 255); // Green
+                textureData[(i * 4096 + j) * 3 + 2] = static_cast<u_int8_t>(color.z * 255); // Blue
+            }
+        }
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 4096, 256, 0, GL_RGB, GL_UNSIGNED_BYTE, textureData.data());
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        // Use the shader program
+        glUseProgram(shaderProgram);
+
+        ImGui::Image(waterfallTexture, ImVec2(widgetSize.x, widgetSize.y), ImVec2(0, 1), ImVec2(1, 0));
+
+        window->DrawList->AddLine(ImVec2(widgetPos.x, widgetPos.y), ImVec2(widgetEndPos.x, widgetEndPos.y), IM_COL32(50, 50, 50, 255), 1.0);
+ 
         //ImU32 bg = ImGui::ColorConvertFloat4ToU32(gui::themeManager.waterfallBg);
         //window->DrawList->AddRectFilled(widgetPos, widgetEndPos, bg);
         //window->DrawList->AddRect(widgetPos, widgetEndPos, IM_COL32(50, 50, 50, 255), 0.0, 0, style::uiScale);
         //window->DrawList->AddLine(ImVec2(widgetPos.x, freqAreaMax.y), ImVec2(widgetPos.x + widgetSize.x, freqAreaMax.y), IM_COL32(50, 50, 50, 255), style::uiScale);
-
 
     ImGui::End();
 }
@@ -84,4 +114,54 @@ void gui::renderTX(float width, float height, float xoffset) {
     ImGui::End();
 }
 
+void gui::initWaterfall() {
+    glGenTextures(1, &waterfallTexture);
 
+    // Shader source code
+    const char* vertexShaderSource = R"(
+    #version 330 core
+    layout(location = 0) in vec2 aPos;
+    layout(location = 1) in vec2 aTexCoord;
+    out vec2 TexCoord;
+    void main() {
+        gl_Position = vec4(aPos, 0.0, 1.0);
+        TexCoord = aTexCoord;
+    }
+    )";
+
+    const char* fragmentShaderSource = R"(
+    #version 330 core
+    out vec4 FragColor;
+    in vec2 TexCoord;
+    uniform sampler2D texture1;
+    void main() {
+        FragColor = texture(texture1, TexCoord);
+    }
+    )";
+
+    // Compile and link shaders
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // Generate texture
+    glBindTexture(GL_TEXTURE_2D, waterfallTexture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    waterfallIndex = 0;
+}
